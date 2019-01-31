5bf890844e84eb494da8c635b70e9e63a220f1cf
Katelyn Bunker <kerbunker@gmail.com>
2019-01-30 20:39:54 -0500

    started main method and set up scanner

--- Project1/src/proj1.java
+++ Project1/src/proj1.java
@@ -1,4 +1,14 @@
-
+import java.util.Scanner;
 public class proj1 {
+	public static void main(String arg[])
+	{
+		Scanner in = new Scanner(System.in);
+		while(in.hasNext()) {
+			
+		}
+		
+		in.close();
+		
+	}
 
 }
