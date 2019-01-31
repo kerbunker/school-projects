b54835489ae0e8b355020212e4fe95d748d7640b
Katelyn Bunker <kerbunker@gmail.com>
2019-01-30 19:46:53 -0500

    Created project

---
+++ Project1/src/proj1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class proj1 {
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		while( in.hasNext() ) {
+			
+		}
+		
+		in.close();
+	}
+
+}
