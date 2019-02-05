d012e46d769040314cc7824961e9376b4820c5b0
Katelyn Bunker <kerbunker@gmail.com>
2019-02-05 10:05:30 -0500

    Start implementing LinkedList class

--- Project1/src/proj1.java
+++ Project1/src/proj1.java
@@ -1,5 +1,28 @@
 import java.util.Scanner;
+
+
+
 public class proj1 {
+	static class LinkedList {
+		Node head; //first item in the list
+		
+		static class Node {
+		    String str;
+		    Node next;
+		    
+		    //Constructor for the node initializing next to null
+		    Node( String s ) {
+		    	str = s;
+		    	next = null;
+		    }
+		    
+		    //Constructor for the node with a next element
+		    Node( String s, Node n ) {
+		    	str = s;
+		    	next = n;
+		    }
+		}
+	}
 	public static void main(String arg[])
 	{
 		Scanner in = new Scanner(System.in);
