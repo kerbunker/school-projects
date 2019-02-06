1fc96a84f9cf2c393f81484d023c9ca63a15c448
Katelyn Bunker <kerbunker@gmail.com>
2019-02-06 12:07:55 -0500

    Encrypt method finished and find by index implemented for linked list

--- Project1/src/proj1.java
+++ Project1/src/proj1.java
@@ -49,17 +49,29 @@ public class proj1 {
 			//if the head has the data, no more nodes need to be searched
 			if( s.compareTo(currentNode.str) == 0 ) {
 				nodeCount++;
+				return nodeCount;
 			} else {
+				nodeCount++;
 				while( currentNode.next != null ) {
 					currentNode = currentNode.next;
+					nodeCount++;
 					if( s.compareTo(currentNode.str) == 0 ) {
-						nodeCount++;
-						break;
+						//nodeCount++;
+						return nodeCount++;
 					}
 				}
 			}
-			return nodeCount;
+			return 0;
 		}
+		public static String findString( LinkedList list, int position )
+        {
+            Node currentNode = list.head;
+            while( position > 1 ) {
+                currentNode = currentNode.next;
+            }
+            return currentNode.str;
+            
+        }
 		
 		public static boolean moveToFront( LinkedList list, String s )
 		{
@@ -87,35 +99,64 @@ public class proj1 {
 	}
 	public static void encrypt( Scanner in, LinkedList list )
 	{
+		System.out.print("0 ");
 		String line;
+		int uncompBytes = 0;
+		int compBytes = 0;
 		while( in.hasNext() ) {
 			line = in.nextLine();
 			int lineLen = line.length();
-			String currentWord = "";
+			StringBuilder currentWord = new StringBuilder();
 			for( int i = 0; i < lineLen; i++ ) {
 				char ch = line.charAt(i);
 				if( Character.isLetter(ch) ) {
-					currentWord.concat( Character.toString(ch) );
+					currentWord.append( ch );
+					//System.out.print( currentWord.toString() );
 				} else {
 					if( currentWord.length() == 0 ) {
 						System.out.print( ch );
+						uncompBytes++;
+						compBytes++;
 					}else {
-						int wordPosition = LinkedList.find( list, currentWord );
+						String word = currentWord.toString();
+						currentWord = new StringBuilder();
+						int wordPosition = LinkedList.find( list, word );
 						if( wordPosition == 0 ) {
-							LinkedList.add( list, currentWord );
-							System.out.print( currentWord );
+							LinkedList.add( list, word );
+							System.out.print( word );
+							uncompBytes += word.length();
+							compBytes += word.length();
 						} else {
-							LinkedList.moveToFront( list, currentWord );
+							LinkedList.moveToFront( list, word );
 							System.out.print( wordPosition );
+							uncompBytes += word.length();
+							compBytes++;
 						}
+						System.out.print( ch );
+						uncompBytes++;
+						compBytes++;
 					}
 				}
 			}
-			//Scanner lineScanner = new Scanner( line );
-			
-			
-			//lineScanner.close();
+			if( currentWord.length() != 0 ) {
+				String word = currentWord.toString();
+				currentWord = new StringBuilder();
+				int wordPosition = LinkedList.find( list, word );
+				if( wordPosition == 0 ) {
+					LinkedList.add( list, word );
+					System.out.print( word );
+					uncompBytes += word.length();
+					compBytes += word.length();
+				} else {
+					LinkedList.moveToFront( list, word );
+					System.out.print( wordPosition );
+					uncompBytes += word.length();
+					compBytes++;
+				}
+			}
 		}
+		System.out.println("0 Uncompressed: " + uncompBytes + " bytes;  Compressed: "
+		+ compBytes + " bytes");
 		
 	}
 	public static void decrypt(Scanner in, LinkedList list )
