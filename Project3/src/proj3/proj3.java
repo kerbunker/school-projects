87da94af20b1ebd554b6d06aa18898fae5e54842
Katelyn Bunker <kerbunker@gmail.com>
2019-04-02 19:36:45 -0400

    Added heap functionality and file reading

--- Project3/src/proj3/proj3.java
+++ Project3/src/proj3/proj3.java
@@ -1,17 +1,42 @@
 package proj3;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class proj3 {
 	
-	public static void main( String args[] )
+	public static Edge heapArray[];
+	public static int heapSize;
+	
+	public static void insert( Edge e )
+	{
+		heapArray[heapSize] = e;
+	}
+	
+	public static void main( String args[] ) throws FileNotFoundException
 	{
 		System.out.println( "Enter the input file: " );
 		Scanner input = new Scanner( System.in );
 		String fileName = input.nextLine();
 		File inputFile = new File( fileName );
+		Scanner fileScanner = new Scanner( inputFile );
+		heapArray = new Edge[ 5000 ];
+		heapSize = 0;
 		
+		while ( fileScanner.hasNext() ) {
+			int current = fileScanner.nextInt();
+			if ( current != -1 ) {
+				int v1 = current;
+				int v2 = fileScanner.nextInt();
+				double w = fileScanner.nextDouble();
+				Edge currentEdge = new Edge( v1, v2, w );
+				insert( currentEdge );
+			}
+				
+		}
+		
+		fileScanner.close();
 		input.close();
 	}
 
